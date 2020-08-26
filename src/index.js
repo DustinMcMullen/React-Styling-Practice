4a436bb60136d788a58b3994473f137157fb2949
DustinMcMullen <51675499+DustinMcMullen@users.noreply.github.com>
2020-08-25 21:13:00 -0500

    Completed challenge Added function to dynamically update greeting & style using JSX

--- src/index.js
+++ src/index.js
@@ -5,3 +5,37 @@
 //Apply the "heading" style in the styles.css
 //Dynamically change the color of the h1 using inline css styles.
 //Morning = red, Afternoon = green, Night = blue.
+import React from "react";
+import ReactDOM from "react-dom";
+
+const date = new Date();
+const hourOfDay = date.getHours();
+console.log(hourOfDay);
+
+const greetingStyle = {
+  color: "black"
+};
+
+function greeting() {
+  if (hourOfDay < 12) {
+    greetingStyle.color = "red";
+    ReactDOM.render(
+      <h1 style={greetingStyle}>Good Morning</h1>,
+      document.getElementById("root")
+    );
+  } else if (hourOfDay >= 12 && hourOfDay < 18) {
+    greetingStyle.color = "green";
+    ReactDOM.render(
+      <h1 style={greetingStyle}>Good Afternoon</h1>,
+      document.getElementById("root")
+    );
+  } else if (hourOfDay >= 18) {
+    greetingStyle.color = "blue";
+    ReactDOM.render(
+      <h1 style={greetingStyle}>Good Night</h1>,
+      document.getElementById("root")
+    );
+  }
+}
+
+greeting();
